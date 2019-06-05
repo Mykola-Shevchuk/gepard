eb0e58054c9c7e42a4fe4f9842594bb02fb0553b
Mykola Shevchuk <mykola.shevchuk@idapgroup.com>
2019-06-06 00:58:07 +0300

    Fixed view of tabs

--- src/js/components/sliders/tabs.js
+++ src/js/components/sliders/tabs.js
@@ -4,39 +4,35 @@ const sliderNode = document.querySelector('.b-tabs-list');
 
 const slider = sliderNode ? tns({
   container: sliderNode,
-  items: 6,
-  edgePadding: 0,
-  animateIn:'tns-fadeIn',
-  animateOut: 'tns-fadeOut',
-  controls: false,
+
+  loop: false,
   nav: false,
+  center: true,
+
+  controls: false,
+
+  slideBy: 'page',
   mouseDrag: true,
 
   responsive: {
-    1300: {
-      items: 6,
-      disable: true,
+    350: {
+      items: 2,
     },
-    800: {
+    450: {
+      items: 3,
+    },
+    550: {
+      items: 4,
+
+    },
+
+    850: {
       items: 6,
       disable: true
-    },
-    600: {
-      items: 4
-    },
-    530: {
-      items: 3
-    },
-    400: {
-      items: 2
-    },
-    300: {
-      items: 1
+
     },
   }
-//    preventScrollOnTouch: 'force',
-//    autoplay: true,
-//    autoplayButtonOutput: false,
+
 }) : null;
 
 export default slider;
